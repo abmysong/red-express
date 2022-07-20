703b0cf284cd323629dd8012a0d388e6554e2b0f
abmysong <abmysong@gmail.com>
2022-07-20 19:52:24 +0900

    [#2] items 정렬 2차

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,7 @@
 const router = global.express.Router();
 const items = global.mocks.items;
 const groceries = global.mocks.groceries;
+const _ = require('lodash');
 
 router.post('/', function(request, response) {
   items.push(request.body);
@@ -11,10 +12,14 @@ router.post('/', function(request, response) {
 });
 
 router.get('/', function(request, response) {
+  const orderItems = _.orderBy(items, request.query.orderByKey, request.query.orderByType);
+
   // TODO: items를 반복해서 groceries에 동일한 uuid가 있는지 확인 하고 있으면 checked = true 값을 넣는다.
-  for (let index = 0; index < items.length; index++) {
-    const item = items[index];
+  for (let index = 0; index < orderItems.length; index++) {
+    const item = orderItems[index];
     console.log(item);
+
+    // groceries 데이터 확인
     const grocery = groceries.find(function(grocery) {
       return grocery.uuid === item.uuid;
     });
@@ -24,10 +29,10 @@ router.get('/', function(request, response) {
       item.checked = false;
     }
   }
-  console.log('Done items get', items);
+  console.log('Done items get', orderItems);
   response.status(200).send({
     result: 'Read',
-    items: items
+    items: orderItems
   });
 });
 
